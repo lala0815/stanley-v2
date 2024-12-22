a0ff10ef390d25935b05a87f7797e5270ad9be78
lala0815 <ken80816ken@gmail.com>
2024-12-22 16:24:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+    https: {
+      key: fs.readFileSync('/etc/letsencrypt/live/stanley.eastasia.cloudapp.azure.com/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/stanley.eastasia.cloudapp.azure.com/cert.pem'),
+    },
   },
 });
